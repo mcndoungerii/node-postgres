b9b6124bbed75d13f5554615e8738a8a4df98f76
Allen <allenmcndounger@gmail.com>
2019-11-30 09:20:17 +0300

    web server is listening on port 8080

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const {Client} = require('pg');
 
 const client = new Client({
     user: "postgres",
-    password: "xxxxxxx",
+    password: "postgres",
     host: "localhost",
     port: 5432,
     database:"duka_server"
